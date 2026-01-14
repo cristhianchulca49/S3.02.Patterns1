595e102f8459167762133bbb37fe3e135c3d7432
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-01-14 18:55:46 +0100

    feat: create Undo class with singleton. addComment, undoComment, showHistory and clearHistory to test

--- src/main/java/Level1/Undo.java
+++ src/main/java/Level1/Undo.java
@@ -1,4 +1,34 @@
 package Level1;
 
+import java.util.List;
+import java.util.Stack;
+
 public class Undo {
+    private Stack<String> history = new Stack<>();
+    private static Undo instance;
+
+    private Undo() {
+    }
+
+    public static Undo getInstance() {
+        if (instance == null) instance = new Undo();
+        return instance;
+    }
+
+    public void addCommand(String command) {
+        history.push(command);
+    }
+
+    public String undoCommand() {
+        if (history.isEmpty()) throw new IllegalStateException("History is empty");
+        return history.pop();
+    }
+
+    public List<String> showHistory() {
+        return List.copyOf(history);
+    }
+
+    public void clearHistory() {
+        history.clear();
+    }
 }
