b6bc59cbdb65bada187f06ac32022ccac43089da
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-01-15 21:23:58 +0100

    refactor: apply correct address and phone format

--- src/main/java/Level2/Countries/Spain/SpainAddress.java
+++ src/main/java/Level2/Countries/Spain/SpainAddress.java
@@ -2,9 +2,9 @@ package Level2.Countries.Spain;
 
 import Level2.Interfaces.Address;
 
- record SpainAddress(String street, String city, String zipCode) implements Address {
+record SpainAddress(String street, String city, String zipCode) implements Address {
     @Override
     public String formatAddress() {
-        return String.format("%s, %s %s", street, city, zipCode);
+        return String.format("%s, %s %s", street, zipCode, city);
     }
 }
--- src/main/java/Level2/Countries/Usa/UsaPhone.java
+++ src/main/java/Level2/Countries/Usa/UsaPhone.java
@@ -5,9 +5,9 @@ import Level2.Interfaces.Phone;
 record UsaPhone(String phone) implements Phone {
     @Override
     public String formatPhone() {
-        return String.format("+1 (0%s) %s-%s",
-                phone.substring(0, 2),
-                phone.substring(2, 5),
-                phone.substring(5, 9));
+        return String.format("+1 (%s) %s-%s",
+                phone.substring(0, 3),
+                phone.substring(3, 6),
+                phone.substring(6, 9));
     }
 }
