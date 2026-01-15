aa5aa5b97c3e224f89dac1c9c6297b986ec7bccc
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-01-16 00:05:30 +0100

    feat: implement ReportServices context to manage ReportStrategy

--- src/main/java/Level3/Services/ReportServices.java
+++ src/main/java/Level3/Services/ReportServices.java
@@ -1,4 +1,22 @@
 package Level3.Services;
 
+import Level3.Domain.ReportData;
+import Level3.Domain.ReportStrategy;
+
+import java.util.List;
+
 public class ReportServices {
+    private ReportStrategy reportStrategy;
+
+    public ReportServices(ReportStrategy strategy) {
+        this.reportStrategy = strategy;
+    }
+
+    public void setStrategy(ReportStrategy strategy) {
+        this.reportStrategy = strategy;
+    }
+
+    public String generateReport(List<ReportData> data) {
+        return reportStrategy.generate(data);
+    }
 }
