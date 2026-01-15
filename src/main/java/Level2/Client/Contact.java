d9cb9fef99b0fb4ee31dcf4246ae01f8f033f6e5
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-01-15 19:46:54 +0100

    feat: feat: implement Contact client class and integrate Abstract Factory logic

--- src/main/java/Level2/Client/Contact.java
+++ src/main/java/Level2/Client/Contact.java
@@ -1,5 +1,15 @@
 package Level2.Client;
 
-public class Contact {
+import Level2.Interfaces.Address;
+import Level2.Interfaces.ContactFactory;
+import Level2.Interfaces.Phone;
 
+public class Contact {
+    Address address;
+    Phone phone;
+
+    Contact(ContactFactory contactFactory) {
+        address = contactFactory.createAddressFormat();
+        phone = contactFactory.createPhoneFormat();
+    }
 }
